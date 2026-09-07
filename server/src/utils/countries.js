ea93b733cfbe9d1c8a62c3e76c2ebbd5a72a5487
kingsley <kingsleyudemesunday@gmail.com>
2026-09-07 17:13:23 +0100

    Fix registration country lookup on Railway

--- server/src/utils/countries.js
+++ server/src/utils/countries.js
@@ -7,7 +7,23 @@ function loadCountries() {
   if (cachedCountries) return cachedCountries;
 
   const clientCountryFile = path.resolve(__dirname, '..', '..', '..', 'client', 'data', 'countries.js');
-  const source = fs.readFileSync(clientCountryFile, 'utf8');
+  let source = '';
+
+  try {
+    source = fs.readFileSync(clientCountryFile, 'utf8');
+  } catch (error) {
+    cachedCountries = {
+      Nigeria: '+234',
+      Ghana: '+233',
+      Kenya: '+254',
+      'South Africa': '+27',
+      'United Kingdom': '+44',
+      'United States': '+1',
+      Canada: '+1'
+    };
+    return cachedCountries;
+  }
+
   const matches = [...source.matchAll(/\['([^']+)',\s*'([^']+)'\]/g)];
 
   cachedCountries = matches.reduce((map, [, name, dialCode]) => {
